215788d94046cd7fd3a4b539ab5e3fab4cd8e4fd
lhyhyl <18181378011@163.com>
2024-01-09 15:01:31 +0800

    更改了数据库的连接方式,目前服务器上的连接方式是验证操作系统

--- blog-server/src/db/seq.js
+++ blog-server/src/db/seq.js
@@ -1,20 +1,48 @@
-const { Sequelize } = require("sequelize")
+// const { Sequelize } = require("sequelize")
 
-const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB } = require("../config/config.default")
+// const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB } = require("../config/config.default")
+
+// const seq = new Sequelize(MYSQL_DB, MYSQL_USER, MYSQL_PASSWORD, {
+//   host: MYSQL_HOST,
+//   dialect: "mysql",
+//   timezone: "+08:00",
+// })
+
+// seq
+//   .authenticate()
+//   .then(() => {
+//     console.log("数据库连接成功")
+//   })
+//   .catch(() => {
+//     console.log("数据库连接失败")
+//   })
+
+// module.exports = seq
+const { Sequelize } = require("sequelize");
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB } = require("../config/config.default");
 
 const seq = new Sequelize(MYSQL_DB, MYSQL_USER, MYSQL_PASSWORD, {
   host: MYSQL_HOST,
   dialect: "mysql",
   timezone: "+08:00",
-})
+  dialectOptions: {
+    socketPath: "/var/run/mysqld/mysqld.sock", // 指定 MySQL socket 路径，根据你的实际情况修改
+  },
+  define: {
+    // 如果数据库中的表没有 createdAt 和 updatedAt 字段，可以将以下选项设为 false
+    timestamps: true,
+  },
+  // 如果采用 'auth_socket' 身份验证插件，需要明确指定 operatorsAliases 为 false
+  operatorsAliases: false,
+});
 
 seq
   .authenticate()
   .then(() => {
-    console.log("数据库连接成功")
+    console.log("数据库连接成功");
   })
-  .catch(() => {
-    console.log("数据库连接失败")
-  })
+  .catch((error) => {
+    console.error("数据库连接失败", error);
+  });
 
-module.exports = seq
+module.exports = seq;
